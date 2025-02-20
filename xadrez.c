3455112e2766ead17e8ddfe8767fa9f4396a1cab
Igor Gomes <gomesworsnoop@gmail.com>
2025-02-20 12:38:58 +0000

    Desafio novato

--- xadrez.c
+++ xadrez.c
@@ -3,30 +3,27 @@
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+// Aluno Igor Gomes
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+int main(){
+    int torre = 1, bispo = 1, rainha = 1;
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    for(int torre = 1; torre <= 5; torre++){
+        printf("\nTorre se moveu para a %dº casa na direita.", torre);
+    }
+    printf("\n");
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    while (bispo <= 5){
+        printf("\nBispo se moveu para a %dº casa por cima, direita.", bispo);
+        bispo++;
+    }
+    printf("\n");
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    do
+    {
+        printf("\nRainha se moveu para a %dº casa na esquerda.", rainha);
+        rainha++;
+    } while (rainha <= 8);
 
     return 0;
 }
